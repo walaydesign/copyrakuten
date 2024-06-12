4857eb09c543af33a172b35a56ec506ff2b00d64
youzhen <yl301114@gmail.com>
2024-06-13 05:08:07 +0800

    排行榜

--- public/js/common.js
+++ public/js/common.js
@@ -2,3 +2,16 @@ $(".topNav_arrow").click(function() {
     $(this).parents(".topNav").find(".topNav_menu").fadeToggle(300);
     $(this).parents(".topNav").toggleClass("active");
 })
+
+$(".hot-topic_title").click(function(){
+    $(this).parents(".hot-topic_item").find(".hot-topic_content").slideToggle(300);
+    $(this).toggleClass("active");
+})
+
+$(".fix-ad_close").click(function(){
+    $(this).parents(".fix-ad").hide();
+})
+
+$(".popup_close").click(function(){
+    $(this).parents(".popup").hide();
+})
